8386fd318a97032e5aabd2410830f0e273d8389b
cvivis <hsy0754@naver.com>
2022-11-07 11:47:37 +0900

    mustache 실습 - pathvariable 적용

---
+++ spring-mushache/src/main/java/com/mushache/bbs/controller/MustacheController.java
@@ -0,0 +1,23 @@
+package com.mushache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "rok");
+        return "greetings";
+    }
+
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("username", "rok");
+        return "greetings";
+    }
+}
