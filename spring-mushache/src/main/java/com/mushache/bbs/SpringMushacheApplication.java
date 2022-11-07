da2f4c3080b23f8ae760c7b148a50df5eabba47a
cvivis <hsy0754@naver.com>
2022-11-07 11:31:34 +0900

    파일 시작

---
+++ spring-mushache/src/main/java/com/mushache/bbs/SpringMushacheApplication.java
@@ -0,0 +1,13 @@
+package com.mushache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMushacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMushacheApplication.class, args);
+    }
+
+}
---
+++ spring-mushache/src/test/java/com/mushache/bbs/SpringMushacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mushache.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMushacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
